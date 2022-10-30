18010910fdbce293e07a778c8f93f9a3cc8bcfd0
yoleo <yoleo@yandex.ru>
2022-10-30 16:08:08 +0200

    Init project

---
+++ src/server/index.ts
@@ -0,0 +1,10 @@
+﻿import Fastify from 'fastify'
+import fastifyStatic from '@fastify/static'
+import path from 'path'
+
+const fastify = Fastify({ logger: true })
+const __dirname = path.resolve()
+
+fastify.register(fastifyStatic, { root: path.join(__dirname, 'public'), prefix: '/public/' })
+fastify.get('/', function (req, reply) { reply.sendFile('index.html') })
+fastify.listen({ port: 3000 }, (err, address) => { if (err) throw err })
